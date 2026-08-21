4714c0cec0e76a05e4b5771c842a8d26952aeb81
harshakalkote <harshakalkote@gmail.com>
2026-08-21 08:54:42 +0530

    Update GEMINI_TEXT_MODELS with new model
    Removed older Gemini model names and added 'gemini-3.5-flash-lite'.

--- js/ai.js
+++ js/ai.js
@@ -242,11 +242,7 @@ Story:`;
    */
   const GEMINI_TEXT_MODELS = [
 	'gemini-3.5-flash',
-    'gemini-2.5-flash',
-    'gemini-flash-latest',
-    'gemini-2.0-flash',
-    'gemini-1.5-flash-latest',
-    'gemini-1.5-flash',
+	'gemini-3.5-flash-lite'
   ];
   const GEMINI_IMAGE_MODELS = [
     'gemini-2.5-flash-image-preview',
